bcf62cfdf80cddb5c12bdec84f379b9136bed490
Zhehao Chen <zchen2404@MBP-chen.lan>
2024-02-04 15:02:13 -0500

    hw

---
+++ Unit4.PetFinder.Starter-main/index.js
@@ -0,0 +1,63 @@
+// import the pets array from data.js
+const pets = require('./data');
+const path = require("path");
+
+// init express app
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+app.use(express.static(path.join(__dirname,"public")));
+
+// GET - / - returns homepage
+app.get('/', (req, res) => {
+    // serve up the public folder as static index.html file
+    res.sendFile(__dirname,"/public/index.html");
+
+});
+
+// hello world route
+app.get('/api', (req, res) => {
+    res.send('Hello World!');
+});
+
+// get all pets from the database
+app.get('/api/v1/pets', (req, res) => {
+    // send the pets array as a response
+    res.send(pets);
+
+});
+
+// get pet by owner with query string
+app.get('/api/v1/pets/owner', (req, res) => {
+    // get the owner from the request
+    const owner = req.params.owner;
+
+
+    // find the pet in the pets array
+    const pet = pets.find(pet => pet.owner === owner);
+
+    // send the pet as a response
+    res.send(pet)
+
+});
+
+// get pet by name
+app.get('/api/v1/pets/:name', (req, res) => {
+    // get the name from the request
+    const name = req.params.name;
+
+
+    // find the pet in the pets array
+    const pet = pets.find(pet => pet.name === name);
+
+    // send the pet as a response
+    res.send(pet);
+
+});
+
+app.listen(PORT, () => {
+    console.log('Server is listening on port ' + PORT);
+});
+
+module.exports = app;
